c593e29d4e2710d3e3286e361bf4dd44a7b7d7e7
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-05-10 12:25:42 +0300

    handle viewports disconnects

--- server/server.js
+++ server/server.js
@@ -3,17 +3,34 @@ var app = require('express').createServer()
 
 var viewports = {}
 
+// TODO find a better way
+// viewports.length doesn't work
+calculateViewports = function() {
+  count = 0;
+  for (var id in viewports) {
+    count++;
+  }
+  return count;
+}
+
 app.listen(5000);
 
 app.get('/', function (req, res) {
   res.contentType('json');
   status = {
-    "name": "zeu"
+    "name": "zeu",
+    "viewports": calculateViewports()
   };
   res.send(status);
 });
 
 io.sockets.on('connection', function (socket) {
+  socket.on('disconnect', function (data) {
+    if (viewports.hasOwnProperty(socket.id)) {
+      delete viewports[socket.id];
+    }
+  });
+
   socket.on('controller_action', function (data) {
     for (var id in viewports) {
       viewports[id].emit('controller_action', data);
@@ -21,7 +38,6 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('viewport_register', function() {
-    console.log('viewport_registerd');
     viewports[socket.id] = socket;
   });
 });
