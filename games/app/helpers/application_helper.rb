65c31d30c962cb12443a07f0f689db81c3189b9f
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-05-16 14:14:49 +0300

    bootstrap

--- games/app/helpers/application_helper.rb
+++ games/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def menu_item name, path
+    klass = current_page?(path) ? " class='active'" : ""
+    "<li#{klass}><a href='#{path}'>#{name}</a></li>".html_safe
+  end
 end
