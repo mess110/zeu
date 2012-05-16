391945261d9e4d45d83bf209c5fc9718c709b160
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-05-16 13:42:36 +0300

    add rails application

---
+++ games/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Games::Application.config.secret_token = '55277f8763f09fa948bb37513a0ba0c1e1c186994a526431ec261413f6301a742ed4231062c19df422b20e1f7d274f16d6918189777cdf1f220df0e7347a760e'
