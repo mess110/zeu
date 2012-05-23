88ae9a0928034006043c17df959033bbbc863fa9
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-05-23 14:13:09 +0300

    game controller

--- games/app/controllers/game_controller.rb
+++ games/app/controllers/game_controller.rb
@@ -1,6 +1,15 @@
 class GameController < ApplicationController
 
+  before_filter :ensure_valid_game_id
+
   def squares
     render :layout => "three"
+  end
+
+  def ensure_valid_game_id
+    server_info = ServerWrapper.info
+    if !server_info['games'].keys.include? params[:id]
+      render :text => "game id invalid"
+    end
   end
 end
